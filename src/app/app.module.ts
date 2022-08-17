e5d46d6a570d16cc915435a76e588e8d23c103a6
Rinae Tshibubudze <vharhee@gmail.com>
2022-08-17 11:21:55 +0200

    data binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { TextcomponentComponent } from './textcomponent/textcomponent.component';
 import { ImagecomponentComponent } from './imagecomponent/imagecomponent.component';
 import { EmpInfoComponent } from './emp-info/emp-info.component';
+import { DataBindingComponent } from './data-binding/data-binding.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TextcomponentComponent,
     ImagecomponentComponent,
-    EmpInfoComponent
+    EmpInfoComponent,
+    DataBindingComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/data-binding/data-binding.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-data-binding',
+  templateUrl: './data-binding.component.html',
+  styleUrls: ['./data-binding.component.css']
+})
+export class DataBindingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  public name = "Interpolation binding"
+  public topic = "Data binding"
+  public value = ""
+  public image = "/assets/Rinae.png"
+
+  onClick() {
+    console.log("Thank you for subscribing")
+  }
+}
